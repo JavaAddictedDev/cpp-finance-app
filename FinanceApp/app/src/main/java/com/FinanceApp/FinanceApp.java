cfedee68f74b307af7927d1641d6c4f399ef793f
Your Name <you@example.com>
2024-12-30 23:33:33 -0300

    Adicionar arquivos de projeto.

---
+++ FinanceApp/app/src/main/java/com/FinanceApp/FinanceApp.java
@@ -0,0 +1,21 @@
+﻿
+package com.FinanceApp;
+
+import android.app.Activity;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class FinanceApp extends Activity
+{
+    /** Chamado quando a atividade é criada pela primeira vez. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        /* Crie um TextView e defina o texto como "Hello World" */
+        TextView  tv = new TextView(this);
+        tv.setText("Hello World!");
+        setContentView(tv);
+    }
+}
